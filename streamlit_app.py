97c62b288742fcd6b6a09d9c0faf430681731c70
sgou1969 <57621083+sgou1969@users.noreply.github.com>
2023-07-06 17:18:07 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,4 @@
-# port 3351
-
-import pandas as pd; import streamlit as st
-
-import streamlit.components.v1 as components
-
-
+import streamlit as st
 
 betAmin = st.number_input('betA minimum', value=3)
 betAmax = st.number_input('betA maximum', value=25)
