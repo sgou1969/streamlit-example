bef739f51464429e2586506a85b9decff8cb24ca
sgou1969 <57621083+sgou1969@users.noreply.github.com>
2023-07-06 17:17:10 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,29 +5,6 @@ import pandas as pd; import streamlit as st
 import streamlit.components.v1 as components
 
 
-with st.form("bet",clear_on_submit=False): 
- 
-    col1, col2, col3,col4,col5= st.columns([3.5,0.5,3.5,1,3.5])
-    
-    with col1:   
-       oddx = st.number_input('odd betA')
-       betAmin = st.number_input('betA minimum', value=3)
-       betAmax = st.number_input('betA maximum', value=25)
-      
-    with col3: 
-       oddy = st.number_input('odd betB')
-       betBmin = st.number_input('betB minimum', value=2)
-       betBmax = st.number_input('betB maximum', value=12)
-    
-    with col4:
-       submitted_check = st.form_submit_button("Calculate")
-          
-    with col5: 
-       oddz = st.number_input('odd betC')
-       betCmin = st.number_input('betC minimum', 2)
-       betCmax = st.number_input('betC maximum', 12)
-         
-    
-  
-       
-   
+
+betAmin = st.number_input('betA minimum', value=3)
+betAmax = st.number_input('betA maximum', value=25)
